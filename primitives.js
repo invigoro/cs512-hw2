2ff8bcf714d0b2bb14bb5b2b7da4948d403abb7d
Tim Wells <invigoro@gmail.com>
2025-10-01 11:23:32 -0600

    color gradient

--- primitives.js
+++ primitives.js
@@ -1,47 +1,35 @@
 // cube
 const cubePositions = new Float32Array([
   -1, -1, -1,  // 0
-   1, -1, -1,  // 1
-   1,  1, -1,  // 2
-  -1,  1, -1,  // 3
-  -1, -1,  1,  // 4
-   1, -1,  1,  // 5
-   1,  1,  1,  // 6
-  -1,  1,  1   // 7
+  1, -1, -1,  // 1
+  1, 1, -1,  // 2
+  -1, 1, -1,  // 3
+  -1, -1, 1,  // 4
+  1, -1, 1,  // 5
+  1, 1, 1,  // 6
+  -1, 1, 1   // 7
 ]);
-
-const cubeColors = new Float32Array([
-  1,0,0,  0,1,0,  0,0,1, 1,1,0, 1,0,1, 0,1,1, 1,1,0, 1,0,1
-]);
-
 
 const cubeIndices = new Uint16Array([
   // Front
-  4, 5, 6,   4, 6, 7,
+  4, 5, 6, 4, 6, 7,
   // Back
-  1, 0, 3,   1, 3, 2,
+  1, 0, 3, 1, 3, 2,
   // Top
-  3, 7, 6,   3, 6, 2,
+  3, 7, 6, 3, 6, 2,
   // Bottom
-  0, 1, 5,   0, 5, 4,
+  0, 1, 5, 0, 5, 4,
   // Right
-  1, 2, 6,   1, 6, 5,
+  1, 2, 6, 1, 6, 5,
   // Left
-  0, 4, 7,   0, 7, 3,
+  0, 4, 7, 0, 7, 3,
 ]);
 
 const tetrahedronPositions = new Float32Array([
   1, 1, 1,    // Vertex 0
- -1, -1, 1,   // Vertex 1
- -1, 1, -1,   // Vertex 2
- 1, -1, -1    // Vertex 3
-]);
-
-const tetrahedronColors = new Float32Array([
-  1, 0, 0,
-  0, 1, 0,
-  0, 0, 1,
-  1, 1, 0
+  -1, -1, 1,   // Vertex 1
+  -1, 1, -1,   // Vertex 2
+  1, -1, -1    // Vertex 3
 ]);
 
 const tetrahedronIndices = new Uint16Array([
@@ -52,7 +40,7 @@ const tetrahedronIndices = new Uint16Array([
 ]);
 
 class buff {
-  constructor(bvbo, bnbo, bibo, len){
+  constructor(bvbo, bnbo, bibo, len) {
     this.vbo = bvbo;
     this.nbo = bnbo;
     this.ibo = bibo;
@@ -60,9 +48,61 @@ class buff {
   }
 }
 
+function interpolateColors(length = 1, minBrightness = 0.1, color1 = null, color2 = null) {
+  //make this a minimum avg brightness of .5 by default
+  let colors = []
+  let c1, c2, c3;
+  let e1, e2, e3;
+  if (color1) {
+    c1 = color1[0]; c2 = color1[1]; c3 = color1[2];
+  }
+  else {
+    do {
+      c1 = Math.random();
+      c2 = Math.random();
+      c3 = Math.random();
+    } while ((c1 + c2 + c3) / 3 < minBrightness)
+  }
+
+  if (color2) {
+    c1 = color2[0]; c2 = color2[1]; c3 = color2[2];
+  }
+  else {
+    do {
+      e1 = Math.random();
+      e2 = Math.random();
+      e3 = Math.random();
+    } while ((e1 + e2 + e3) / 3 < minBrightness)
+  }
+  for (i = 0; i < length; i++) {
+    //interpolate between the two random colors
+    i1 = c1 + ((e1 - c1) / length) * (i + 1);
+    i2 = c2 + ((e2 - c2) / length) * (i + 1);
+    i3 = c3 + ((e3 - c3) / length) * (i + 1);
+    colors.push(i1, i2, i3);
+  }
+  return new Float32Array(colors);
+}
+
+function createCube(radius = 1) {
+  return {
+    positions: cubePositions.map(x => x * radius),
+    colors: interpolateColors(cubePositions.length),
+    indices: cubeIndices,
+  };
+}
+
+function createTetrahedron(radius = 1) {
+  return {
+    positions: tetrahedronPositions.map(x => x * radius),
+    colors: interpolateColors(tetrahedronPositions.length),
+    indices: tetrahedronIndices,
+  };
+}
+
+
 function createCylinder(radius = 1, height = 2, segments = 32) {
   const positions = [];
-  const colors = [];
   const indices = [];
 
   // Generate vertices
@@ -73,20 +113,16 @@ function createCylinder(radius = 1, height = 2, segments = 32) {
 
     // Top circle
     positions.push(x, height / 2, z);
-    colors.push(Math.random(), Math.random(), Math.random());
 
     // Bottom circle
     positions.push(x, -height / 2, z);
-    colors.push(Math.random(), Math.random(), Math.random());
   }
 
   // Center points for caps
   const topCenterIndex = positions.length / 3;
   positions.push(0, height / 2, 0);
-  colors.push(1, 1, 1);
   const bottomCenterIndex = positions.length / 3;
   positions.push(0, -height / 2, 0);
-  colors.push(1, 1, 1);
 
   // Side indices
   for (let i = 0; i < segments; i++) {
@@ -116,14 +152,13 @@ function createCylinder(radius = 1, height = 2, segments = 32) {
 
   return {
     positions: new Float32Array(positions),
-    colors: new Float32Array(colors),
+    colors: interpolateColors(positions.length),
     indices: new Uint16Array(indices)
   };
 }
 
 function createSphere(radius = 1, latBands = 24, longBands = 24) {
   const positions = [];
-  const colors = [];
   const indices = [];
 
   for (let lat = 0; lat <= latBands; lat++) {
@@ -141,7 +176,6 @@ function createSphere(radius = 1, latBands = 24, longBands = 24) {
       const z = radius * sinPhi * sinTheta;
 
       positions.push(x, y, z);
-      colors.push(Math.random(), Math.random(), Math.random());
     }
   }
 
@@ -157,19 +191,17 @@ function createSphere(radius = 1, latBands = 24, longBands = 24) {
 
   return {
     positions: new Float32Array(positions),
-    colors: new Float32Array(colors),
+    colors: interpolateColors(positions.length),
     indices: new Uint16Array(indices)
   };
 }
 
 function createCone(radius = 1, height = 2, segments = 32) {
   const positions = [];
-  const colors = [];
   const indices = [];
 
   const apexIndex = 0;
   positions.push(0, height / 2, 0); // Apex
-  colors.push(1, 1, 1);
 
   // Circle vertices
   for (let i = 0; i < segments; i++) {
@@ -177,12 +209,10 @@ function createCone(radius = 1, height = 2, segments = 32) {
     const x = radius * Math.cos(theta);
     const z = radius * Math.sin(theta);
     positions.push(x, -height / 2, z);
-    colors.push(Math.random(), Math.random(), Math.random());
   }
 
   const baseCenterIndex = positions.length / 3;
   positions.push(0, -height / 2, 0); // center of base
-  colors.push(1, 1, 1);
 
   // Side triangles
   for (let i = 1; i <= segments; i++) {
@@ -198,14 +228,13 @@ function createCone(radius = 1, height = 2, segments = 32) {
 
   return {
     positions: new Float32Array(positions),
-    colors: new Float32Array(colors),
+    colors: interpolateColors(positions.length),
     indices: new Uint16Array(indices)
   };
 }
 
 function createTorus(majorRadius = 1, minorRadius = 0.3, majorSegments = 32, minorSegments = 16) {
   const positions = [];
-  const colors = [];
   const indices = [];
 
   for (let i = 0; i <= majorSegments; i++) {
@@ -223,7 +252,6 @@ function createTorus(majorRadius = 1, minorRadius = 0.3, majorSegments = 32, min
       const z = (majorRadius + minorRadius * cosPhi) * sinTheta;
 
       positions.push(x, y, z);
-      colors.push(Math.random(), Math.random(), Math.random());
     }
   }
 
@@ -239,7 +267,7 @@ function createTorus(majorRadius = 1, minorRadius = 0.3, majorSegments = 32, min
 
   return {
     positions: new Float32Array(positions),
-    colors: new Float32Array(colors),
+    colors: interpolateColors(positions.length),
     indices: new Uint16Array(indices)
   };
 }
